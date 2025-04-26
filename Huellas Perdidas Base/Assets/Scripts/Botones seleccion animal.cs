affdc190969487876f389e9bfc7b03d8825359f6
LorenaCU20 <est.jiseth.cardenas@unimilitar.edu.co>
2025-04-26 15:03:53 -0500

    Creación escenas 1

--- Huellas Perdidas Base/Assets/Scripts/Botones seleccion animal.cs
+++ Huellas Perdidas Base/Assets/Scripts/Botones seleccion animal.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Botonesseleccionanimal : MonoBehaviour
 {
@@ -10,11 +11,13 @@ public class Botonesseleccionanimal : MonoBehaviour
         if (GUI.Button(rectButton, textobutton))
         {
             Debug.Log("Menú Perros");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
         var textobutton2 = new GUIContent("Gato");
         var rectButton2 = new Rect(600, 300, 100, 50);
         if (GUI.Button(rectButton2, textobutton2))
         {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
             Debug.Log("Menú Gatos");
         }
     }
