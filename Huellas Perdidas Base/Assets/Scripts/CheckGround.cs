2873f2e0df337c0cbbc701397ee062847dbc2a00
Sebas1408 <sebasfandisalama1408@gmail.com>
2025-05-21 20:49:13 -0500

    Arreglos fondo nivel 1

--- Huellas Perdidas Base/Assets/Scripts/CheckGround.cs
+++ Huellas Perdidas Base/Assets/Scripts/CheckGround.cs
@@ -3,15 +3,5 @@ using UnityEngine;
 public class CheckGround : MonoBehaviour
 {
   
-    public static bool isGrounded;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        isGrounded = true;
-    }
-
-    public void OnTriggerExit2D(Collider2D collision)
-    {
-        isGrounded = false;
-    }
 }
--- Huellas Perdidas Base/Assets/Scripts/MovimientoPlayer.cs
+++ Huellas Perdidas Base/Assets/Scripts/MovimientoPlayer.cs
@@ -1,39 +1,7 @@
 using UnityEngine;
 
-public class MovimientoPlayer : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
-    public float runSpeed = 2;
-    public float jumpSpeed = 3;
-    Rigidbody2D rb2D;
-    public SpriteRenderer spriteRenderer;
-
-    void Start()
-    {
-        rb2D = GetComponent<Rigidbody2D>();
-    }
-
-    void FixedUpdate()
-    {
-        Vector2 nuevaVelocidad = rb2D.linearVelocity;
-
-        if (Input.GetKey("d") || Input.GetKey("right"))
-        {
-            nuevaVelocidad.x = runSpeed;
-            spriteRenderer.flipX = false;
-        }
-        else if (Input.GetKey("a") || Input.GetKey("left"))
-        {
-            nuevaVelocidad.x = -runSpeed;
-            spriteRenderer.flipX = true;
-        }
-        else
-        {
-            nuevaVelocidad.x = 0;
-        }
-
-        if (Input.GetKey("w") && CheckGround.isGrounded)
-        {
-            nuevaVelocidad.y = jumpSpeed;
-        }
-    }
+   
+    
 }
