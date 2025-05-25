27f303d0e012ac273a0102c0dc9810814abb33af
Carobm10 <est.laurac.beltranm@unimilitar.edu.co>
2025-05-25 10:00:50 -0500

    Limpieza de archivos rotos y preparación para normalizar saltos

--- Huellas Perdidas Base/Assets/Scripts/Niveles/FrutCollected.cs
+++ Huellas Perdidas Base/Assets/Scripts/Niveles/FrutCollected.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 
 public class FrutCollected : MonoBehaviour
 {
+    public string tipoDeFruta; // "Fresa" o "Manzana"
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            // Contador
+            if (FruitCounter.instancia != null)
+            {
+                FruitCounter.instancia.AgregarFruta(tipoDeFruta);
+            }
+
+            // Efecto visual
             GetComponent<SpriteRenderer>().enabled = false;
-            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(0).gameObject.SetActive(true);
             Destroy(gameObject, 0.5f);
         }
     }
