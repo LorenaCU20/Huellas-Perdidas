f6e6da5043ff5349c91d56caa73b7d08a3801e9d
Carobm10 <est.laurac.beltranm@unimilitar.edu.co>
2025-05-25 11:42:02 -0500

    Bola de Fuego NIvel 3

---
+++ Huellas Perdidas Base/Assets/Scripts/Niveles/EnemigoRaton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class EnemigoRaton : MonoBehaviour
+{
+    public void Morir()
+    {
+        // Aquí podrías añadir una animación, sonido, etc.
+        Destroy(gameObject);
+    }
+}
